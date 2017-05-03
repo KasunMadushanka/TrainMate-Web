b794892ba8f14d9dc63d0a6dba401d853c4d860a
Kasun <kmadushanka40@gmail.com>
2017-05-03 19:48:26 +0530

    first commit

--- routes/admin.js
+++ routes/admin.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET login page. */
 router.get('/', function (req, res, next) {
-    res.render('admin/login');
+    res.render('admin/index');
 });
 
 router.get('/index', function (req, res, next) {
