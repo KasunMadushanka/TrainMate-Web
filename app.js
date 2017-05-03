1683ebd2fddac47e0cbb18d7bd09e4e4a5993d72
Kasun <kmadushanka40@gmail.com>
2017-05-03 18:44:56 +0530

    first commit

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(session({secret:"trainMATEs3c43tK3y",resave:false,saveUninitialized:true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
